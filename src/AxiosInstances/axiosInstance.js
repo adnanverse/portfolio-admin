ae71e8974c1a06b82a8a3714befa8b683aefe0ea
Mohd Adnan <adnandev098@gmail.com>
2025-07-18 17:03:48 +0530

    change instance url

--- src/AxiosInstances/axiosInstance.js
+++ src/AxiosInstances/axiosInstance.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-    baseURL: 'http://localhost:4000/'
-    // import.meta.env.VITE_API_BASE_URL
+    baseURL:  import.meta.env.VITE_API_BASE_URL
 })
 
 // ✅ Interceptor lagate hain: ye har request se pehle chalega
